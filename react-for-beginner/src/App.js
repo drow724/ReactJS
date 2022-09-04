ad75d08db693a11518671c1b1f7ff787571e0c92
Macintosh <drow724@naver.com>
2022-09-04 22:41:14 +0900

    To Do List part Two

--- react-for-beginner/src/App.js
+++ react-for-beginner/src/App.js
@@ -13,13 +13,15 @@ function App() {
     setToDos((currentArray) => [toDo, ...currentArray]);
     setTodo("");
   }
-  console.log(toDos)
+
   return <div>
     <h1>My To Dos {toDos.length}</h1>
     <form onSubmit={onSubmit}>
       <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..." />
       <button>Add to Do</button>
     </form>
+    <hr />
+    {toDos.map((item, index) => <li key={index}>{item}</li>)}
   </div>
 }
 
