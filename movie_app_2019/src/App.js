9205d774541a89dca0916607518fbb8d0f18550f
Macintosh <drow724@naver.com>
2022-09-12 11:33:56 +0900

    Building the Router

--- movie_app_2019/src/App.js
+++ movie_app_2019/src/App.js
@@ -1,7 +1,19 @@
-import React from "react";
+import {
+    HashRouter as Router,
+    Routes,
+    Route,
+  } from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home";
 
 function App () {
-return <span></span>
-}
+return (
+    <Router>
+       <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+        </Routes>
+    </Router>
+)}
 
 export default App;
